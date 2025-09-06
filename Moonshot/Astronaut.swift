fb983b90dbdadc40631b8af73f4224c02a7ce7b1
Kurczaak <kurczakmichal97@gmail.com>
2025-09-06 14:33:50 +0200

    feat: add  basic implementation of moonshot's home page

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  astronaut.swift
+//  Moonshot
+//
+//  Created by Michał Kurczak on 06/09/2025.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable, Equatable{
+    let id: String
+    let name: String
+    let description: String
+}
