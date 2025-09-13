a2f7c4a131a7712ebee1b5bc07ba4b1ff429260c
Kurczaak <kurczakmichal97@gmail.com>
2025-09-13 11:40:53 +0200

    feat: implement astronaut and mission views

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Michał Kurczak on 13/09/2025.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
